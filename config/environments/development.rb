4e608575c0bd19fdcbb1348ffe9373424038f320
mjennings754 <mitchellj@outlook.jp>
2026-01-22 16:02:45 +1100

    add groups scaffold

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,6 +11,8 @@ Rails.application.configure do
 
   # Show full error reports.
   config.consider_all_requests_local = true
+  
+  config.debug_exception_response_format = :api
 
   # Enable server timing.
   config.server_timing = true
